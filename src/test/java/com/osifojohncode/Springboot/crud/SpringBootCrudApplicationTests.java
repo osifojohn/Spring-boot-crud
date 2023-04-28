ba838568030d3c9b4b864f825e9ee5bf8c2629b0
Osifo John <osifojohntec@gmail.com>
2023-04-28 14:34:36 +0100

    first commit

---
+++ src/test/java/com/osifojohncode/Springboot/crud/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osifojohncode.Springboot.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
