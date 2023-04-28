5dc7b4c52d67e16081636275eb04b96f50fc9625
Osifo John <osifojohntec@gmail.com>
2023-04-28 18:00:34 +0100

    implementing delete department by id

--- src/main/java/com/osifojohncode/Springboot/crud/controller/DepartmentController.java
+++ src/main/java/com/osifojohncode/Springboot/crud/controller/DepartmentController.java
@@ -3,10 +3,7 @@ package com.osifojohncode.Springboot.crud.controller;
 import com.osifojohncode.Springboot.crud.entity.Department;
 import com.osifojohncode.Springboot.crud.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,14 @@ public class DepartmentController {
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
         return departmentService.fetchDepartmentList();
+    }
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById( departmentId);
+        return "Department deleted successfully !!!";
     }
 }
