8ce1049372ae8f806648c20419cb2f6366e9e4dc
Osifo John <osifojohntec@gmail.com>
2023-04-28 21:56:58 +0100

    implementing data validation for post request

--- src/main/java/com/osifojohncode/Springboot/crud/controller/DepartmentController.java
+++ src/main/java/com/osifojohncode/Springboot/crud/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package com.osifojohncode.Springboot.crud.controller;
 
 import com.osifojohncode.Springboot.crud.entity.Department;
 import com.osifojohncode.Springboot.crud.service.DepartmentService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +13,7 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
       return departmentService.saveDepartment(department);
     }
     @GetMapping("/departments")
