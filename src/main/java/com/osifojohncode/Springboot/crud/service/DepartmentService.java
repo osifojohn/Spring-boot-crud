4a4d4c8a171a762bbe2946b4ea2374790ba96a37
Osifo John <osifojohntec@gmail.com>
2023-04-28 16:49:17 +0100

    implementing get all departments

--- src/main/java/com/osifojohncode/Springboot/crud/service/DepartmentService.java
+++ src/main/java/com/osifojohncode/Springboot/crud/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.osifojohncode.Springboot.crud.service;
 
 import com.osifojohncode.Springboot.crud.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
  public Department saveDepartment(Department department);
+
+ public List<Department> fetchDepartmentList();
 }
