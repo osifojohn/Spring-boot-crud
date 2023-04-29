a7e7dc51370ead30011d8e64ad4db340e0e52ff7
Osifo John <osifojohntec@gmail.com>
2023-04-29 14:29:03 +0100

    added project to mysql database

--- src/main/java/com/osifojohncode/Springboot/crud/error/RestResposeEntityExceptionHandler.java
+++ src/main/java/com/osifojohncode/Springboot/crud/error/RestResposeEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.lang.module.ResolutionException;
 
-@ControllerAdvice
+@ControllerAdvice 
 @ResponseStatus
 public class RestResposeEntityExceptionHandler
         extends ResponseEntityExceptionHandler {
