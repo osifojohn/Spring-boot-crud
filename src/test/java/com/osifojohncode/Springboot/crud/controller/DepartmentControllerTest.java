6f34fbe2f2c78439d882d0c57be45651aa1ca62e
Osifo John <osifojohntec@gmail.com>
2023-04-30 17:38:43 +0100

    updating controller unit test

--- src/test/java/com/osifojohncode/Springboot/crud/controller/DepartmentControllerTest.java
+++ src/test/java/com/osifojohncode/Springboot/crud/controller/DepartmentControllerTest.java
@@ -5,16 +5,21 @@ import com.osifojohncode.Springboot.crud.service.DepartmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(DepartmentController.class)
 class DepartmentControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
+
     @MockBean
     private DepartmentService departmentService;
 
@@ -22,31 +27,45 @@ class DepartmentControllerTest {
 
     @BeforeEach
     void setUp() {
-        department =
-                Department.builder()
+        department = Department.builder()
                         .departmentName("Geomatics")
                         .departmentCode("GEM - 011")
                         .departmentAddress("Benin")
-                        .departmentId(1L)
-                        .build();
+                .departmentId(1L)
+                .build();
     }
 
     @Test
-    void saveDepartment() {
-        Department inputDepartment =
-                Department.builder()
-                        .departmentName("Geomatics")
-                        .departmentCode("GEM - 011")
-                        .departmentAddress("Benin")
-                        .build();
+    void saveDepartment() throws Exception {
+        Department inputDepartment = Department.builder()
+                .departmentAddress("Ugbowo")
+                .departmentCode("CVE-022")
+                .departmentName("Civil")
+                .build();
 
         Mockito.when(departmentService.saveDepartment(inputDepartment))
                 .thenReturn(department);
 
+        mockMvc.perform(post("/departments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "\t\"departmentName\":\"Civil\",\n" +
+                                "\t\"departmentAddress\":\"Ugbowo\",\n" +
+                                "\t\"departmentCode\":\"CVE-022\"\n" +
+                                "}"))
+                .andExpect(status().isOk());
 
     }
 
     @Test
-    void fetchDepartmentById() {
+    void fetchDepartmentById() throws Exception {
+        Mockito.when(departmentService.fetchDepartmentById(1L))
+                .thenReturn(department);
+
+        mockMvc.perform(get("/departments/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.departmentName").
+                        value(department.getDepartmentName()));
     }
 }
